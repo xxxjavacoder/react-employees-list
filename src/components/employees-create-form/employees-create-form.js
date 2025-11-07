10f82d3374ac19c976a14545f28ca3d6715555ae
lytvyniuk_v <lv@pinpools.com>
2025-11-07 15:57:57 +0100

    Fix.

--- src/components/employees-create-form/employees-create-form.js
+++ src/components/employees-create-form/employees-create-form.js
@@ -1,23 +1,51 @@
+import React from 'react';
+
 import './employees-create-form.css';
 
-const EmployeesCreateForm = () => {
-    return (
-        <div className="app-create-form">
-            <h3>Add new employee</h3>
-            <form className="add-form d-flex">
-                <input type="text"
-                       className="form-control new-post-label"
-                       placeholder="What is his/her name?" />
-                <input type="number"
-                       className="form-control new-post-label"
-                       placeholder="Salary in dollars $?" />
+class EmployeesCreateForm extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            name: '',
+            salary: '',
+        }
+    }
 
-                <button type="submit" className="btn btn-outline-light">
-                    Add
-                </button>
-            </form>
-        </div>
-    )
+    onValueChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    render(){
+        const {name, salary} = this.state;
+
+        return (
+            <div className="app-create-form">
+                <h3>Add new employee</h3>
+                <form className="add-form d-flex">
+                    <input type="text"
+                           name="name"
+                           value={name}
+                           className="form-control new-post-label"
+                           placeholder="What is his/her name?"
+                            onChange={this.onValueChange}
+                    />
+                    <input type="number"
+                           name="salary"
+                           value={salary}
+                           className="form-control new-post-label"
+                           placeholder="Salary in dollars $?"
+                            onChange={this.onValueChange}
+                    />
+
+                    <button type="submit" className="btn btn-outline-light">
+                        Add
+                    </button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default EmployeesCreateForm;
